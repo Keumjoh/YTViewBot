ce6b612c7223c9b50b0dade998fbe7ee8b00285b
Keumjoh <54800909+Keumjoh@users.noreply.github.com>
2019-10-09 16:03:30 +0800

    Add files via upload

---
+++ YTViewBot/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace YTViewBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblh = new System.Windows.Forms.Label();
+            this.lblm = new System.Windows.Forms.Label();
+            this.lbls = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txttime = new System.Windows.Forms.TextBox();
+            this.txturl = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblproxy = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtfile = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblh);
+            this.groupBox1.Controls.Add(this.lblm);
+            this.groupBox1.Controls.Add(this.lbls);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txttime);
+            this.groupBox1.Controls.Add(this.txturl);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 178);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Config";
+            // 
+            // lblh
+            // 
+            this.lblh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblh.Location = new System.Drawing.Point(158, 131);
+            this.lblh.Name = "lblh";
+            this.lblh.Size = new System.Drawing.Size(54, 23);
+            this.lblh.TabIndex = 10;
+            this.lblh.Text = "0h";
+            this.lblh.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblm
+            // 
+            this.lblm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblm.Location = new System.Drawing.Point(214, 131);
+            this.lblm.Name = "lblm";
+            this.lblm.Size = new System.Drawing.Size(54, 23);
+            this.lblm.TabIndex = 9;
+            this.lblm.Text = "0m";
+            this.lblm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbls
+            // 
+            this.lbls.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbls.Location = new System.Drawing.Point(270, 131);
+            this.lbls.Name = "lbls";
+            this.lbls.Size = new System.Drawing.Size(54, 23);
+            this.lbls.TabIndex = 8;
+            this.lbls.Text = "0s";
+            this.lbls.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(203, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Time Elasped";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Refresh Time (1000ms = 1s)";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(222, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "STOP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "START";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Check URL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Youtube Video URL";
+            // 
+            // txttime
+            // 
+            this.txttime.Location = new System.Drawing.Point(9, 134);
+            this.txttime.Name = "txttime";
+            this.txttime.Size = new System.Drawing.Size(139, 20);
+            this.txttime.TabIndex = 1;
+            this.txttime.Text = "30000";
+            this.txttime.TextChanged += new System.EventHandler(this.Txttime_TextChanged);
+            this.txttime.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txttime_KeyPress);
+            // 
+            // txturl
+            // 
+            this.txturl.Location = new System.Drawing.Point(9, 43);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(315, 20);
+            this.txturl.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Controls.Add(this.lblproxy);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtfile);
+            this.groupBox2.Location = new System.Drawing.Point(12, 196);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(330, 369);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Proxy Config - TXT file with list of proxies";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 148);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(318, 215);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // lblproxy
+            // 
+            this.lblproxy.AutoSize = true;
+            this.lblproxy.Location = new System.Drawing.Point(91, 117);
+            this.lblproxy.Name = "lblproxy";
+            this.lblproxy.Size = new System.Drawing.Size(33, 13);
+            this.lblproxy.TabIndex = 14;
+            this.lblproxy.Text = "Proxy";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Current Proxy :";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 35);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Select File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "File Location";
+            // 
+            // txtfile
+            // 
+            this.txtfile.Location = new System.Drawing.Point(9, 42);
+            this.txtfile.Name = "txtfile";
+            this.txtfile.ReadOnly = true;
+            this.txtfile.Size = new System.Drawing.Size(315, 20);
+            this.txtfile.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowWebBrowserDrop = false;
+            this.webBrowser1.Location = new System.Drawing.Point(348, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(566, 553);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(926, 577);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YTViewBot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txttime;
+        private System.Windows.Forms.TextBox txturl;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblproxy;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtfile;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbls;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblh;
+        private System.Windows.Forms.Label lblm;
+    }
+}
+
---
+++ YTViewBot/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SetProxy;
+
+namespace YTViewBot
+{
+    public partial class Form1 : Form
+    {
+        private static List<WeakReference> __ENCList;
+        static Form1()
+        {
+            Form1.__ENCList = new List<WeakReference>();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Form1.__ENCAddToList(this);
+
+        }
+        private static void __ENCAddToList(object value)
+        {
+            lock (Form1.__ENCList)
+            {
+                if (Form1.__ENCList.Count == Form1.__ENCList.Capacity)
+                {
+                    int item = 0;
+                    int count = checked(Form1.__ENCList.Count - 1);
+                    for (int i = 0; i <= count; i = checked(i + 1))
+                    {
+                        if (Form1.__ENCList[i].IsAlive)
+                        {
+                            if (i != item)
+                            {
+                                Form1.__ENCList[item] = Form1.__ENCList[i];
+                            }
+                            item = checked(item + 1);
+                        }
+                    }
+                    Form1.__ENCList.RemoveRange(item, checked(Form1.__ENCList.Count - item));
+                    Form1.__ENCList.Capacity = Form1.__ENCList.Count;
+                }
+                Form1.__ENCList.Add(new WeakReference(RuntimeHelpers.GetObjectValue(value)));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            _ReadDataGrid();
+            webBrowser1.Refresh();
+        }
+        private void _ReadDataGrid()
+        {
+            if (dataGridView1.Rows.Count >= 1)
+            {
+                int cROW = dataGridView1.SelectedRows[0].Index;
+                if (cROW < dataGridView1.RowCount)
+                {
+                    dataGridView1.Rows[++cROW].Selected = true;
+                    lblproxy.Text = dataGridView1.Rows[cROW].Cells[0].Value.ToString();
+                    WinInetInterop.SetConnectionProxy(lblproxy.Text);
+                    webBrowser1.Navigate(new Uri(txturl.Text));
+                }
+            }
+            else
+            {
+                MessageBox.Show("no proxy");
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opFile = new OpenFileDialog();
+            opFile.Title = "Select TXT with proxy list";
+            opFile.Filter = "TXT files (*.txt) | *.txt;";
+
+            if (opFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    txtfile.Text = opFile.FileName;
+
+                    //Read the data from text file
+                    string[] textData = System.IO.File.ReadAllLines(@"" + txtfile.Text);
+                    string[] headers = textData[0].Split(',');
+
+                    //Create and populate DataTable
+                    DataTable dataTable1 = new DataTable();
+                    foreach (string header in headers)
+                        dataTable1.Columns.Add(header, typeof(string), null);
+                    for (int i = 0; i < textData.Length; i++)
+                        dataTable1.Rows.Add(textData[i]);
+
+                    //Set the DataSource of DataGridView to the DataTable
+                    dataGridView1.DataSource = dataTable1;
+
+                    lblproxy.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+
+                    MessageBox.Show("NUMBER OF PROXY:" + dataGridView1.Rows.Count.ToString());
+
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("Unable to open file " + exp.Message);
+                }
+            }
+            else
+            {
+                opFile.Dispose();
+            }
+        }
+
+        private void Txttime_TextChanged(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txttime.Text))
+            {
+                if (Convert.ToInt32(txttime.Text) <= 29999)
+                {
+                    MessageBox.Show("Required 30000ms");
+                }
+            }
+            else
+            {
+                timer1.Interval = Convert.ToInt32(txttime.Text);
+            }
+
+        }
+
+        private void Txttime_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txturl.Text))
+            {
+                webBrowser1.Navigate(new Uri(txturl.Text));
+            }
+            else
+            {
+                MessageBox.Show("Please input your url");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txturl.Text))
+            {
+                if(dataGridView1.Rows.Count >= 1)
+                {
+                    WinInetInterop.SetConnectionProxy(lblproxy.Text);
+                    webBrowser1.Navigate(new Uri(txturl.Text));
+                    timer1.Start();
+                    timer2.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Please input your proxy");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please input your url");
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+        }
+        int sec = 0;
+        int min = 0;
+        int hour = 0;
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+
+            sec++;
+            if(sec >= 60)
+            {
+                min++;
+                sec = 0;
+            }
+            if(min >= 60)
+            {
+                hour++;
+                min = 0;
+            }
+            lbls.Text = sec.ToString() + "s";
+            lblm.Text = min.ToString() + "m";
+            lblh.Text = hour.ToString() + "h";
+        }
+    }
+}
